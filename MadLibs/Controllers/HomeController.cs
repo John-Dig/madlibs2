d1270acccf462683e6a2a803a227b78e4bbf1a3b
John Diggins <rohnfiggins@gmail.com>
2023-02-27 11:39:34 -0800

    works
    Co-authored-by: James Provance <jb24x7@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -1,15 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
-// using FriendLetter.Models;
+using MadLibs.Models;
 
 namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/hello")]
-    public string Hello() { return "Hello Muffin!"; }
-    [Route("/goodbye")]
-    public string Goodbye() { return "Goodbye you silly Muffin!!!"; }
-  
+    [Route("/")]
+    public ActionResult TheGoofyAdventure()
+    {
+      MadLibsVariable myMadLibsVariable = new MadLibsVariable();
+
+      myMadLibsVariable.Name = "Richard";
+      myMadLibsVariable.Noun1 = "rock";
+      myMadLibsVariable.Noun2 = "stick";
+      myMadLibsVariable.Verb = "fly";
+      myMadLibsVariable.Adjective = "righteous";
+      myMadLibsVariable.FoodItem = "General Tso's Chicken";
+
+      return View(myMadLibsVariable);
+
+    }
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+    [Route("/result")]
+    public ActionResult Result(string name, string noun1, string noun2, string verb, string adjective, string fooditem)
+    {
+      MadLibsVariable myMadLibsVariable = new MadLibsVariable();
+      myMadLibsVariable.Name = name;
+      myMadLibsVariable.Noun1 = noun1;
+      myMadLibsVariable.Noun2 = noun2;
+      myMadLibsVariable.Verb = verb;
+      myMadLibsVariable.Adjective = adjective;
+      myMadLibsVariable.FoodItem = fooditem;
+      return View(myMadLibsVariable);
+    }
   }
 
 
--- MadLibs/Models/MadLibsVariable.cs
+++ MadLibs/Models/MadLibsVariable.cs
@@ -2,7 +2,12 @@ namespace MadLibs.Models
 {
   public class MadLibsVariable
   {
-    
+    public string Name { get; set;}
+    public string Noun1 { get; set;}
+    public string Noun2 { get; set;}
+    public string Adjective { get; set;}
+    public string FoodItem { get; set;}
+    public string Verb { get; set;}
 
   }
 }
